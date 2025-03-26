93f38a5c8e6d16beded64b4362f96d88f17ece49
Viet Nguyen <vietnguyen2358@gmail.com>
2025-03-26 09:49:13 -0700

    Show Date Range Selector depending on bound or unbound

--- src/client/app/components/ChartDataSelectComponent.tsx
+++ src/client/app/components/ChartDataSelectComponent.tsx
@@ -6,18 +6,24 @@ import * as React from 'react';
 import { MeterOrGroup } from '../types/redux/graph';
 import MeterAndGroupSelectComponent from './MeterAndGroupSelectComponent';
 import UnitSelectComponent from './UnitSelectComponent';
+import { useAppSelector } from '../redux/reduxHooks';
+import { selectQueryTimeInterval} from '../redux/slices/graphSlice';
+import DateRangeComponent from './DateRangeComponent';
 
 /**
  * A component which allows the user to select which data should be displayed on the chart.
  * @returns Chart data select element
  */
 export default function ChartDataSelectComponent() {
+	const queryTimeInterval = useAppSelector(selectQueryTimeInterval);
+	const isBounded = queryTimeInterval.getIsBounded();
 
 	return (
 		<div>
 			<MeterAndGroupSelectComponent meterOrGroup={MeterOrGroup.groups} />
 			<MeterAndGroupSelectComponent meterOrGroup={MeterOrGroup.meters} />
 			<UnitSelectComponent />
+			{isBounded && <DateRangeComponent />}
 		</div>
 	);
 }
