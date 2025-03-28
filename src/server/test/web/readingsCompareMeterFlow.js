a7dbe9c813676f721d6b193600eb2d5d6a338e25
Steven Huss-Lederman <huss@beloit.edu>
2025-03-28 09:08:56 -0500

    fix C19 description
    This changes kWh to kW per the updated design
    doc information. Until very recently it had the wrong
    info. This was my fault and not the developers
    who did this work.

--- src/server/test/web/readingsCompareMeterFlow.js
+++ src/server/test/web/readingsCompareMeterFlow.js
@@ -306,7 +306,7 @@ mocha.describe('readings API', () => {
 
 				});
 	
-				mocha.it('C19: 7 day shift end 2022-11-01 15:00:00 (beyond data) for 15 minute reading intervals and quantity units & kWh as kWh', async () => {
+				mocha.it('C19: 7 day shift end 2022-11-01 15:00:00 (beyond data) for 15 minute reading intervals and quantity units & kW as kW', async () => {
 					const unitData = [
 						{
 							// u4
