3ccf4cf1dab50bd62023556de5bda3ce82096a7b
vyvyvyThao <wn73@drexel.edu>
2025-04-02 17:26:19 -0400

    update LR8

--- src/server/test/web/readingsLineMeterRangeFlow.js
+++ src/server/test/web/readingsLineMeterRangeFlow.js
@@ -11,83 +11,84 @@ const { chai, mocha, app } = require('../common');
 const Unit = require('../../models/Unit');
 const { prepareTest,
     parseExpectedCsv,
-    expectReadingToEqualExpected,
+    expectRangeToEqualExpected,
     getUnitId,
     ETERNITY,
     METER_ID } = require('../../util/readingsUtils');
 
-const rdPath = 'src/server/test/web/readingsData/';
-
 mocha.describe('readings API', () => {
     mocha.describe('readings test, test if data returned by API is as expected', () => {
         mocha.describe('for line charts', () => {
-            mocha.describe('for flow meters', () => {
-                mocha.it('LR8: should have daily points for 15 minute reading intervals and flow units with +-inf start/end time & kW as kW', async () => {
-                    const unitData = [
-                        {
-                            // u4
-                            name: 'kW', 
-                            identifier: '', 
-                            unitRepresent: Unit.unitRepresentType.FLOW, 
-                            secInRate: 3600, 
-                            typeOfUnit: Unit.unitType.UNIT, 
-                            suffix: '', 
-                            displayable: Unit.displayableType.ALL, 
-                            preferredDisplay: true, 
-                            note: 'kilowatts' 
-                        },
-                        { 
-                            // u5
-                            name: 'Electric', identifier: '', 
-                            unitRepresent: Unit.unitRepresentType.FLOW, 
-                            secInRate: 3600, 
-                            typeOfUnit: Unit.unitType.METER, 
-                            suffix: '', 
-                            displayable: Unit.displayableType.NONE, 
-                            preferredDisplay: false, 
-                            note: 'special unit' 
-                        }
-                    ];
-                    const conversionData = [
-                        {
-                            // c4
-                            sourceName: 'Electric',
-                            destinationName: 'kW',
-                            bidirectional: false,
-                            slope: 1,
-                            intercept: 0,
-                            note: 'Electric → kW'
-                        }
-                    ];
-                    const meterData = [
-                        {
-                            name: 'Electric kW',
-                            unit: 'Electric',
-                            defaultGraphicUnit: 'kW',
-                            displayable: true,
-                            gps: undefined,
-                            note: 'special meter',
-                            file: `${rdPath}readings_ri_15_days_75.csv`,
-                            deleteFile: false,
-                            readingFrequency: '15 minutes',
-                            id: METER_ID
-                        }
-                    ];
+			mocha.describe('for range (min/max)', () => {		
+				mocha.describe('for flow meters', () => {
+					mocha.it('LR8: should have daily points for 15 minute reading intervals and flow units with +-inf start/end time & kW as kW', async () => {
+						const unitData = [
+							{
+								// u4
+								name: 'kW', 
+								identifier: '', 
+								unitRepresent: Unit.unitRepresentType.FLOW, 
+								secInRate: 3600, 
+								typeOfUnit: Unit.unitType.UNIT, 
+								suffix: '', 
+								displayable: Unit.displayableType.ALL, 
+								preferredDisplay: true, 
+								note: 'kilowatts' 
+							},
+							{ 
+								// u5
+								name: 'Electric', 
+								identifier: '', 
+								unitRepresent: Unit.unitRepresentType.FLOW, 
+								secInRate: 3600, 
+								typeOfUnit: Unit.unitType.METER, 
+								suffix: '', 
+								displayable: Unit.displayableType.NONE, 
+								preferredDisplay: false, 
+								note: 'special unit' 
+							}
+						];
+						const conversionData = [
+							{
+								// c4
+								sourceName: 'Electric',
+								destinationName: 'kW',
+								bidirectional: false,
+								slope: 1,
+								intercept: 0,
+								note: 'Electric → kW'
+							}
+						];
+						const meterData = [
+							{
+								name: 'Electric kW',
+								unit: 'Electric',
+								defaultGraphicUnit: 'kW',
+								displayable: true,
+								gps: undefined,
+								note: 'special meter',
+								file: 'test/web/readingsData/readings_ri_15_days_75.csv',
+								deleteFile: false,
+								readingFrequency: '15 minutes',
+								id: METER_ID
+							}
+						];
 
-                    await prepareTest(unitData, conversionData, meterData);
+						await prepareTest(unitData, conversionData, meterData);
 
-                    const unitId = await getUnitId('kW');
-                    const expected = await parseExpectedCsv(`${rdPath}expected_line_range_ri_15_mu_kW_gu_kW_st_-inf_et_inf.csv`)
-                    
-                    const res = await chai.request(app).get(`api/unitReadings/line/meters/${METER_ID}`)
-                        .query({timeInterval: ETERNITY.toString(), graphicUnitId: unitId});
-                    expectReadingToEqualExpected(res, expected)
-                })
+						const unitId = await getUnitId('kW');
+						const expected = await parseExpectedCsv(`src/server/test/web/readingsData/expected_line_range_ri_15_mu_kW_gu_kW_st_-inf_et_inf.csv`);
+						
+						const res = await chai.request(app).get(`/api/unitReadings/line/meters/${METER_ID}`)
+							.query({timeInterval: ETERNITY.toString(), graphicUnitId: unitId});
+						expectRangeToEqualExpected(res, expected);
+					});
 
                 // Add LR25 here
 
                 // Add LR26 here
-            });
+            	});
+			});
         });
     });
 });
